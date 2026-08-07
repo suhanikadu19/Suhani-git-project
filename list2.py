033aa394b520966702080d8e1ff3a1816622d785
suhanikadu19 <suhanikadu07@gmail.com>
2026-08-07 15:36:20 +0530

    lists type

---
+++ list2.py
@@ -0,0 +1,12 @@
+list = [100,50,400,500]
+print(list)
+list[1]=200
+print("After chaging:",list)
+list.append(600)
+print("After append:",list)
+list.insert(2,300)
+print("After Inserting :",list)
+list.remove(600)
+print("After remove: ",list)
+list.pop(0)
+print("After remove element by zeroth index: ",list)
