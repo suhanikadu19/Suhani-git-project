455ea68d73126ad90d58e15c594a3a1111ffd03c
suhanikadu19 <suhanikadu07@gmail.com>
2026-08-07 15:02:28 +0530

    first program of list

---
+++ input.py
@@ -0,0 +1,12 @@
+numbers = [10,20,30,40,50]
+third = numbers[2]
+print("Third element: ",third)
+length = len(numbers)
+print("Length of list: ",list)
+if len(numbers)==0:
+    print("List is empty")
+else:
+    print("List is not empty")
+
+
+
