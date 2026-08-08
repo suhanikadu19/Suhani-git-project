17d0702576f206e2586ec80532490f750dc67a65
suhanikadu19 <suhanikadu07@gmail.com>
2026-08-08 18:59:01 +0530

    sum of list number

---
+++ listsum.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50]
+
+total_sum = sum(numbers)
+average = total_sum / len(numbers)
+
+print("Total Sum:", total_sum)  # Output: 150
+print("Average:", average)  # Output: 30.0
