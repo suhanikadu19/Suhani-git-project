5f21819107800565ff04791695a6c57f2cb4a2ab
suhanikadu19 <suhanikadu07@gmail.com>
2026-08-12 19:29:05 +0530

    implement dictionary creation from two equal-length lists using zip

---
+++ map_lists_to_dict.py
@@ -0,0 +1,7 @@
+keys = ["name", "age", "city"]
+values = ["Bob", 25, "London"]
+
+# Mapping two lists into a dictionary using zip()
+result_dict = dict(zip(keys, values))
+
+print("Mapped Dictionary:", result_dict)
