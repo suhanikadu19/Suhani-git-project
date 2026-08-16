049c7c4f0935135e4214edc4f1d182c480798b8a
suhanikadu19 <suhanikadu07@gmail.com>
2026-08-16 16:00:31 +0530

    simple calculator

---
+++ any.py
@@ -0,0 +1,12 @@
+def simplecalc(a,b=100 ):
+	
+#fill in the missing code..	
+	print("addition:",a+b)
+	print("subtraction:",a-b)
+	print("multiplication:",a*b)
+
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+simplecalc(a = num1)
+
+simplecalc(a=num1, b=num2 )
